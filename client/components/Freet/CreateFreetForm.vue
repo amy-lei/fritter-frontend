a682f7d80337012f337bd258f411cbc09e3c75e3
Amy <leiamy12@gmail.com>
2022-10-30 18:28:53 -0400

    Refactor freet comp and freet create into base component

--- client/components/Freet/CreateFreetForm.vue
+++ client/components/Freet/CreateFreetForm.vue
@@ -1,11 +1,18 @@
 <!-- Form for creating freets (block style) -->
 
+<template>
+  <CreateForm
+    :placeholder="'What\'s happening'"
+    :actionLabel="'Freet'"
+    :request="request"
+  />
+</template>
 <script>
-import BlockForm from '@/components/common/BlockForm.vue';
+import CreateForm from '@/components/common/CreateForm.vue';
 
 export default {
   name: 'CreateFreetForm',
-  mixins: [BlockForm],
+  components: {CreateForm},
   data() {
     return {
       url: '/api/freets',
@@ -22,6 +29,30 @@ export default {
         setTimeout(() => this.$delete(this.alerts, message), 3000);
       }
     };
+  },
+  methods: {
+    async request(params) {
+      /**
+       * Submits a request to the freet's endpoint
+       * @param params - Options for the request
+       * @param params.options - Request options
+       * @param params.successCallback - Function to run if the the request succeeds
+       * @param params.failureCallback - Function to run if the the request fails
+       */
+      try {
+        const r = await fetch(`/api/freets`, params.options);
+        if (!r.ok) {
+          const res = await r.json();
+          throw new Error(res.error);
+        }
+
+        this.$store.commit('refreshFreets');
+        params.successCallback();
+      } catch (e) {
+        params.failureCallback(e);
+      }
+    }
+
   }
 };
 </script>
--- client/components/Freet/FreetComponent.vue
+++ client/components/Freet/FreetComponent.vue
@@ -2,71 +2,20 @@
 <!-- We've tagged some elements with classes; consider writing CSS using those classes to style them... -->
 
 <template>
-  <article
-    class="freet"
-  >
-    <header>
-      <h3 class="author">
-        @{{ freet.author }}
-      </h3>
-      <div
-        v-if="$store.state.username === freet.author"
-        class="actions"
-      >
-        <button
-          v-if="editing"
-          @click="submitEdit"
-        >
-          ✅ Save changes
-        </button>
-        <button
-          v-if="editing"
-          @click="stopEditing"
-        >
-          🚫 Discard changes
-        </button>
-        <button
-          v-if="!editing"
-          @click="startEditing"
-        >
-          ✏️ Edit
-        </button>
-        <button @click="deleteFreet">
-          🗑️ Delete
-        </button>
-      </div>
-    </header>
-    <textarea
-      v-if="editing"
-      class="content"
-      :value="draft"
-      @input="draft = $event.target.value"
-    />
-    <p
-      v-else
-      class="content"
-    >
-      {{ freet.content }}
-    </p>
-    <p class="info">
-      Posted at {{ freet.dateModified }}
-      <i v-if="freet.edited">(edited)</i>
-    </p>
-    <section class="alerts">
-      <article
-        v-for="(status, alert, index) in alerts"
-        :key="index"
-        :class="status"
-      >
-        <p>{{ alert }}</p>
-      </article>
-    </section>
-  </article>
+  <Post
+    :post="freet"
+    :request="request"
+    :isPrivate="false"
+  />
 </template>
 
 <script>
+
+import Post from '@/components/common/Post.vue';
+
 export default {
   name: 'FreetComponent',
+  components: {Post},
   props: {
     // Data from the stored freet
     freet: {
@@ -74,70 +23,14 @@ export default {
       required: true
     }
   },
-  data() {
-    return {
-      editing: false, // Whether or not this freet is in edit mode
-      draft: this.freet.content, // Potentially-new content for this freet
-      alerts: {} // Displays success/error messages encountered during freet modification
-    };
-  },
   methods: {
-    startEditing() {
-      /**
-       * Enables edit mode on this freet.
-       */
-      this.editing = true; // Keeps track of if a freet is being edited
-      this.draft = this.freet.content; // The content of our current "draft" while being edited
-    },
-    stopEditing() {
-      /**
-       * Disables edit mode on this freet.
-       */
-      this.editing = false;
-      this.draft = this.freet.content;
-    },
-    deleteFreet() {
-      /**
-       * Deletes this freet.
-       */
-      const params = {
-        method: 'DELETE',
-        callback: () => {
-          this.$store.commit('alert', {
-            message: 'Successfully deleted freet!', status: 'success'
-          });
-        }
-      };
-      this.request(params);
-    },
-    submitEdit() {
-      /**
-       * Updates freet to have the submitted draft content.
-       */
-      if (this.freet.content === this.draft) {
-        const error = 'Error: Edited freet content should be different than current freet content.';
-        this.$set(this.alerts, error, 'error'); // Set an alert to be the error text, timeout of 3000 ms
-        setTimeout(() => this.$delete(this.alerts, error), 3000);
-        return;
-      }
-
-      const params = {
-        method: 'PATCH',
-        message: 'Successfully edited freet!',
-        body: JSON.stringify({content: this.draft}),
-        callback: () => {
-          this.$set(this.alerts, params.message, 'success');
-          setTimeout(() => this.$delete(this.alerts, params.message), 3000);
-        }
-      };
-      this.request(params);
-    },
     async request(params) {
       /**
        * Submits a request to the freet's endpoint
        * @param params - Options for the request
        * @param params.body - Body for the request, if it exists
-       * @param params.callback - Function to run if the the request succeeds
+       * @param params.successCallback - Function to run if the the request succeeds
+       * @param params.failurreCallback - Function to run if the the request fails
        */
       const options = {
         method: params.method, headers: {'Content-Type': 'application/json'}
@@ -153,23 +46,14 @@ export default {
           throw new Error(res.error);
         }
 
-        this.editing = false;
         this.$store.commit('refreshFreets');
 
-        params.callback();
+        params.successCallback();
       } catch (e) {
-        this.$set(this.alerts, e, 'error');
-        setTimeout(() => this.$delete(this.alerts, e), 3000);
+        params.failureCallback(e);
+
       }
     }
   }
 };
 </script>
-
-<style scoped>
-.freet {
-    border: 1px solid #111;
-    padding: 20px;
-    position: relative;
-}
-</style>
